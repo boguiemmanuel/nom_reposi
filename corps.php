a0f177e17344999ada8bde8f65fb151bedf19b59
boguiemmanuel <94707939+boguiemmanuel@users.noreply.github.com>
2021-11-23 03:33:29 +0000

    Add files via upload

---
+++ corps.php
@@ -0,0 +1,80 @@
+<div id="contenu">
+    <div class="cont">
+    <div class="lb">
+        <h1>chef de groupe</h1>
+        <img src="foto/15.JPG" alt="" class="img5">
+
+       Lorem ipsum dolor sit amet consectetur, adipisicing <br> 
+    elit. Officiis eaque accusantium eveniet omnis quibusdam, <br>
+     magnam non! Soluta excepturi exercitationem nam quibusdam <br>
+      illo dolorem <br>
+     animi! At accusantium id sunt nobis mollitia. <br>
+     
+    </div>
+    <div class="lb">
+        <h1>ALICIA</h1>
+        <img src="foto/14.jpg" alt="" class="img5">
+        Lorem ipsum dolor sit amet consectetur, adipisicing <br> 
+    elit. Officiis eaque accusantium eveniet omnis quibusdam, <br>
+     magnam non! Soluta excepturi exercitationem nam quibusdam <br>
+      illo dolorem <br>
+     animi! At accusantium id sunt nobis mollitia. <br>
+    
+    </div>
+    <div class="lb">
+    <h1>IRIE</h1>
+    <img src="" alt="">
+    Lorem ipsum dolor sit amet consectetur, adipisicing <br> 
+    elit. Officiis eaque accusantium eveniet omnis quibusdam, <br>
+     magnam non! Soluta excepturi exercitationem nam quibusdam <br>
+      illo dolorem <br>
+     animi! At accusantium id sunt nobis mollitia. <br>
+    
+    </div>
+
+
+
+    </div>
+</div>
+
+<div id="contenu">
+    <div class="cont">
+    <div class="lb">
+    <h2> new</h2>
+    <select name="" id="">
+       <option value="">EDUCATION</option>
+       <option value="">SPORT</option>
+       <option value="">DEVOIRS</option>
+       <option value="">FORMATION</option>
+       </select>
+       Lorem ipsum dolor sit amet consectetur, adipisicing <br> 
+    elit. Officiis eaque accusantium eveniet omnis quibusdam, <br>
+     magnam non! Soluta excepturi exercitationem nam quibusdam <br>
+      illo dolorem <br>
+     animi! At accusantium id sunt nobis mollitia. <br>
+        <input type="submit" value="RECHERCHE">
+     
+    </div>
+    <div class="lb">
+        <h1>liste classe</h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing <br> 
+    elit. Officiis eaque accusantium eveniet omnis quibusdam, <br>
+     magnam non! Soluta excepturi exercitationem nam quibusdam <br>
+      illo dolorem <br>
+     animi! At accusantium id sunt nobis mollitia. <br>
+        <input type="submit" value="voir liste">
+    
+    </div>
+    <div class="lb">
+    <h1>club info</h1>
+    Lorem ipsum dolor sit amet consectetur, adipisicing <br> 
+    elit. Officiis eaque accusantium eveniet omnis quibusdam, <br>
+     magnam non! Soluta excepturi exercitationem nam quibusdam <br>
+      illo dolorem <br>
+     animi! At accusantium id sunt nobis mollitia. <br>
+    <input type="submit"  value="voir club">
+    </div>
+
+
+    </div>
+</div>
