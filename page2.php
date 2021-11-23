a30d27353783aa746000190e1a248129e1769f2c
boguiemmanuel <94707939+boguiemmanuel@users.noreply.github.com>
2021-11-23 03:29:44 +0000

    Add files via upload

---
+++ page2.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="men.css">
+     <title>Document</title>
+</head>
+<body>
+<?php include('menu.php');?>
+<div align="center">
+<p>
+     <img src="foto/9.jpg" alt="" class="img5" >
+     Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br>
+      Iure deleniti quod dolorem perspiciatis, aliquid nesciunt <br> 
+      quasi nemo velit molestias voluptates necessitatibus <br>
+       sed commodi <br>
+     debitis sapiente qui voluptatum! Amet, voluptas? Et. <br>
+</p>
+</div>
+<div class="mg">
+<p>
+<img src="foto/10.jpg" alt="" class="img5">
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br>
+      Iure deleniti quod dolorem perspiciatis, aliquid nesciunt <br> 
+      quasi nemo velit molestias voluptates necessitatibus <br>
+       sed commodi <br>
+     debitis sapiente qui voluptatum! Amet, voluptas? Et. <br>
+</p>
+</div>
+
+<div>
+</p>
+     <img src="foto/11.jpg" alt="" class="img5">
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br>
+      Iure deleniti quod dolorem perspiciatis, aliquid nesciunt <br> 
+      quasi nemo velit molestias voluptates necessitatibus <br>
+       sed commodi <br>
+     debitis sapiente qui voluptatum! Amet, voluptas? Et. <br>
+</p>
+</div>
+
+<div class="jp">
+</p>
+     <img src="foto/12.jpg" alt="" class="img5">
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br>
+      Iure deleniti quod dolorem perspiciatis, aliquid nesciunt <br> 
+      quasi nemo velit molestias voluptates necessitatibus <br>
+       sed commodi <br>
+     debitis sapiente qui voluptatum! Amet, voluptas? Et. <br>
+</p>
+</div>
+
+<div class="jp">
+</p>
+     <img src="foto/13.jpg" alt="" class="img5">
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br>
+      Iure deleniti quod dolorem perspiciatis, aliquid nesciunt <br> 
+      quasi nemo velit molestias voluptates necessitatibus <br>
+       sed commodi <br>
+     debitis sapiente qui voluptatum! Amet, voluptas? Et. <br>
+</p>
+</div>
+<div class="je">
+</p>
+     <img src="foto/5.jpg" alt="" class="img5">
+      ipsum dolor sit amet consectetur adipisicing elit. <br>
+      Iure deleniti quod dolorem perspiciatis, aliquid nesciunt <br> 
+      quasi nemo velit molestias voluptates necessitatibus <br>
+       sed commodi <br>
+     debitis sapiente qui voluptatum! Amet, voluptas? Et. <br>
+</p>
+</div>
+
+<footer>
+        <p>
+            copyright &copy; IDA /2A 2021-2022
+        </p>
+</footer>
+
+    
+</body>
+</html>
